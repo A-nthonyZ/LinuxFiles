f17d933467f1199ec59e24cd2802d8a89bff77a3
az <ajzook3@gamil.com>
2022-03-09 17:21:49 -0500

    various added messages

--- MintSetup.sh
+++ MintSetup.sh
@@ -17,6 +17,10 @@ sudo apt remove \
 	hexchat-common \
 	hypnotix
 
+#fix clock
+timedatectl set-local-rtc 1
+
+
 #grahical settings
 gsettings set org.cinnamon.desktop.interface gtk-theme 'Mint-Y-Dark-Purple'
 gsettings set org.cinnamon.desktop.interface icon-theme 'Mint-Y-Dark-Purple'
@@ -25,11 +29,38 @@ gsettings set org.cinnamon.desktop.interface cursor-theme 'DMZ-Black'
 gsettings set org.cinnamon.theme name 'Mint-Y-Dark-Purple'
 gsettings set org.cinnamon.desktop.interface clock-use-24h false
 gsettings set org.cinnamon.desktop.interface clock-show-date true
+gsettings set org.cinnamon show-media-keys-osd 'small'
 
     #change background
     mkdir /home/$USER/Pictures/wallpapers/
     cp images/surface-pro-6-3840x2160_785686-mm-90.jpg /home/$USER/Pictures/wallpapers/SurfacePro6.jpg
-    gsettings set org.cinnamon.desktop.background picture-uri '/home/$USER/Pictures/wallpapers/SurfacePro6.jpg'
+    gsettings set org.cinnamon.desktop.background picture-uri 'file:///home/server/Pictures/wallpapers/SurfacePro6.jpg'
+
+    #terminal
+    dconf load /org/gnome/terminal/legacy/profiles:/ < ~/Documents/git/LinuxFiles/files/terminalProfiles.dconf
+    dconf load /org/gnome/terminal/legacy/profiles:/:4a3418a3-ac81-4621-a0a8-54aaf030d792/ \
+    < ~/Documents/git/LinuxFiles/files/myTerminalProfile.dconf
+    
+#shortcut keys
+    #Lock Screen
+    gsettings set org.cinnamon.desktop.keybindings.media-keys screensaver "['<Super>l']"
+    gsettings set org.cinnamon.desktop.keybindings looking-glass-keybinding "['<Primary><Alt>l']"
+
+
+#panel
+gsettings set org.cinnamon panels-height "['1:40']"
+gsettings set org.cinnamon panel-zone-symbolic-icon-sizes '[{"panelId": 1, "left": 28, "center": 28, "right": 20}]'
+gsettings set org.cinnamon enabled-applets "['panel1:left:0:menu@cinnamon.org:14', 'panel1:left:2:grouped-window-list@cinnamon.org:16', 'panel1:right:3:xapp-status@cinnamon.org:18', 'panel1:right:9:notifications@cinnamon.org:19', 'panel1:right:4:printers@cinnamon.org:20', 'panel1:right:0:removable-drives@cinnamon.org:21', 'panel1:right:6:network@cinnamon.org:24', 'panel1:right:7:sound@cinnamon.org:25', 'panel1:right:8:power@cinnamon.org:26', 'panel1:right:2:systray@cinnamon.org:44', 'panel1:right:1:keyboard@cinnamon.org:45', 'panel1:right:10:calendar@cinnamon.org:46']"
+
+
+#desktop icons
+gsettings set org.nemo.desktop trash-icon-visible true
+gsettings set org.nemo.desktop home-icon-visible false
+gsettings set org.nemo.desktop computer-icon-visible false
+gsettings set org.nemo.desktop show-orphaned-desktop-icons false
+
+#hot corners
+gsettings set org.cinnamon hotcorner-layout "['expo:false:0', 'desktop:false:0', 'scale:false:0', 'desktop:true:300 ']"
 
 #sound settings
 gsettings set org.cinnamon.sounds login-enabled false
@@ -39,6 +70,7 @@ gsettings set org.cinnamon.sounds notification-enabled true
 gsettings set org.cinnamon.sounds tile-enabled false
 gsettings set org.cinnamon.desktop.sound maximum-volume 200
 
+
 #install new software
 
     #Github cli install
@@ -47,4 +79,7 @@ gsettings set org.cinnamon.desktop.sound maximum-volume 200
     sudo apt-add-repository https://cli.github.com/packages
     sudo apt update
     sudo apt install gh
+
+    #pia install
+    sh ~/Documents/git/LinuxFiles/files/pia-linux-3.3-06906.run
 
